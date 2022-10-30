1e8c6261d61c66d8ff6a322053bba12f0d983aca
firatcanulukaya <mua.firat@gmail.com>
2022-10-30 19:39:58 +0200

    create getcountry action

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 /* import '../scss/styles.js'; // scss yazacagın tum sayafalrda kullan */
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import "./App.css";
 import Detail from "./components/detail/Detail";
 import Nav from "./components/nav/Nav.jsx";
 
 
-
 function App() {
-  return (
-    <div className="App bg-[#e5e7eb] w-full h-full">
-      <div className="flex flex-col">
-      <Routes>
-        <Route  path="/" element={ <Nav/>}/>
-        <Route path ="detail" element={ <Detail/> }/>
-        </Routes>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App bg-[#e5e7eb] w-full h-full">
+            <div className="flex flex-col">
+                <Routes>
+                    <Route path="/" element={<Nav/>}/>
+                    <Route path="/detail/:name" element={<Detail/>}/>
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/items/Items.jsx
+++ src/components/items/Items.jsx
@@ -6,19 +6,19 @@ function Items(props) {
     <div className="flex flex-col   justify-center items-center ">
       <div className="bg-white rounded-2xl gap-2  w-[34rem] sm:w-[40rem] h-[8rem] flex items-center">
         <Link
-          to="/detail"
+          to={`/detail/${props.cca2.toLowerCase()}`} //tıklayacağımız linki ülkenin koduna göre değiştiriyoruz
           className="flex items-center  justify-start ml-[2rem] gap-[2.5rem]  dark:text-black"
         >
           <img className="w-[7rem] h-[5rem] " src={props.flags.png} />
-          <b>{props.name.common}</b>  
+          <b>{props.name.common.length > 15 ? `${props.name.common.substring(0,15)}...` : props.name.common}</b>
           <div className="grid grid-rows-2 grid-flow-col gap-1">
             <p className="text-slate-300 text-xs"> region</p>
             <p >{props.continents}</p>
-            <p className="text-slate-300 text-xs"> Are km</p>
+            <p className="text-slate-300 text-xs"> Area km</p>
             <p>{props.population}</p>
             <p className="text-slate-300 text-xs">population</p>
             <p>{props.area}</p>
-          
+
           </div>
         </Link>
       </div>
